b27a183c36775515341e1f4b45a5f7629e8acacb
vishaal-gaddipati <vgaddipati@ucsd.edu>
2022-04-22 10:54:06 -0700

    javac -cp ".;lib\junit-4.13.2.jar;lib\hamcrest-core-1.3.jar" MarkdownParseTest.java java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore MarkdownParseTest

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,8 +1,26 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
 public class MarkdownParseTest {
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    public void test1() {
+        Path fileName = Path.of("test-file.md");
+        try {
+            String content = Files.readString(fileName);
+            ArrayList<String> links = MarkdownParse.getLinks(content);
+            assertEquals("[https://something.com, some-page.html]", links);
+        }
+        catch (IOException e) {
+            return;
+        }
     }
 }
