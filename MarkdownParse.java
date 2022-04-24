d69b93e65b80d92a8908a7bed63945325c1cb467
vishaal-gaddipati <vgaddipati@ucsd.edu>
2022-04-23 17:39:38 -0700

    no paren fix

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -21,7 +21,13 @@ public class MarkdownParse {
                 break;
             }
             int openParen = markdown.indexOf("(", closeBracket);
+            if (openParen == -1) {
+                break;
+            }
             int closeParen = markdown.indexOf(")", openParen);
+            if (closeParen == -1) {
+                break;
+            }
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
         }
