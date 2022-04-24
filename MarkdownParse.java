95eb99c3c3729304a5ba277b236ef44917b474eb
vishaal-gaddipati <vgaddipati@ucsd.edu>
2022-04-23 17:52:21 -0700

    image fix

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -12,6 +12,13 @@ public class MarkdownParse {
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
+            if (markdown.indexOf("!", currentIndex) > 0) {
+                int openBracket = markdown.indexOf("[", currentIndex);
+                int closeBracket = markdown.indexOf("]", openBracket);
+                int openParen = markdown.indexOf("(", closeBracket);
+                int closeParen = markdown.indexOf(")", openParen);
+                currentIndex = closeParen + 1;
+            }
             int openBracket = markdown.indexOf("[", currentIndex);
             if (openBracket == -1) {
                 break;
