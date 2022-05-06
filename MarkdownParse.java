0dbc221b94cf290e2cad037e677b21a188c34fa0
Vishaal H Gaddipati <cs15lsp22ack@ieng6-203.ucsd.edu>
2022-05-06 11:22:38 -0700

    test

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -8,6 +8,8 @@ import java.util.ArrayList;
 public class MarkdownParse {
 
     public static ArrayList<String> getLinks(String markdown) {
+        //test edit
+
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
