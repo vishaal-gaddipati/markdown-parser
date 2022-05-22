a2de6f3b28e0efda01dfb12a64b5f527cfa16b25
vishaal-gaddipati <vgaddipati@ucsd.edu>
2022-05-22 16:37:12 -0700

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -80,4 +80,22 @@ public class MarkdownParseTest {
         ArrayList<String> links = MarkdownParse.getLinks(content);
         assertEquals(List.of("url.com", "google.com", "ucsd.edu"), links);
     }
+
+    @Test
+    public void testSnippet2() throws IOException {
+        Path fileName = Path.of("C:\\Users\\visha\\Documents\\GitHub\\markdown-parser\\snippet2.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(content);
+        assertEquals(List.of("a.com", "b.com", "a.com", "example.com"), links);
+    }
+
+    @Test
+    public void testSnippet3() throws IOException {
+        Path fileName = Path.of("C:\\Users\\visha\\Documents\\GitHub\\markdown-parser\\snippet3.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(content);
+        assertEquals(List.of("https://www.twitter.com", 
+        "https://sites.google.com/eng.ucsd.edu/cse-15l-spring-2022/schedule", 
+        "https://cse.ucsd.edu/"), links);
+    }
 }
